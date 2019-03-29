180eabde12f06ce15e8eae8665e5ea191c6ec755
probaldas <49045586+probaldas@users.noreply.github.com>
2019-03-29 07:27:28 -0500

    Setting up the RecyclerView

--- app/src/main/java/com/funnow/mygitapp/MainActivity.java
+++ app/src/main/java/com/funnow/mygitapp/MainActivity.java
@@ -1,10 +1,17 @@
 package com.funnow.mygitapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private CommitViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +19,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setTitle(R.string.app_title);
+
+        viewModel = ViewModelProviders.of(this).get(CommitViewModel.class);
+
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
     }
 }
