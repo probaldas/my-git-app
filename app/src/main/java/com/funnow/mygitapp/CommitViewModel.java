f5b7256ef0193f0547c2b41fbd40bfdd844506f1
probaldas <49045586+probaldas@users.noreply.github.com>
2019-03-29 00:12:27 -0500

    Created ViewModel and Binding for the layout file

---
+++ app/src/main/java/com/funnow/mygitapp/CommitViewModel.java
@@ -0,0 +1,34 @@
+package com.funnow.mygitapp;
+
+import androidx.lifecycle.ViewModel;
+
+public class CommitViewModel extends ViewModel {
+
+    private String committerName;
+    private String commitHash;
+    private String commitMsg;
+
+    public String getCommitterName() {
+        return committerName;
+    }
+
+    public void setCommitterName(String committerName) {
+        this.committerName = committerName;
+    }
+
+    public String getCommitHash() {
+        return commitHash;
+    }
+
+    public void setCommitHash(String commitHash) {
+        this.commitHash = commitHash;
+    }
+
+    public String getCommitMsg() {
+        return commitMsg;
+    }
+
+    public void setCommitMsg(String commitMsg) {
+        this.commitMsg = commitMsg;
+    }
+}
