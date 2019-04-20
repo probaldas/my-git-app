d44163f1ca27d5889b5353c90a73d090f866ca70
probaldas <49045586+probaldas@users.noreply.github.com>
2019-04-20 17:57:24 -0500

    Created GitApiDataSource to replace GitApiRepository

--- app/src/main/java/com/funnow/mygitapp/services/WebService.java
+++ app/src/main/java/com/funnow/mygitapp/services/WebService.java
@@ -7,10 +7,17 @@ import java.util.List;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
+import retrofit2.http.Query;
 
 public interface WebService {
 
     @GET("repos/probaldas/my-git-app/commits")
     Call<List<GitCommits>> getCommits(@Header("Accept") String contentType);
+
+    @GET("repos/probaldas/my-git-app/commits")
+    Call<List<GitCommits>> getCommits(
+            @Header("Accept") String contentType,
+            @Query("page") int pageNumber,
+            @Query("per_page") int commitsPerPage);
 
 }
